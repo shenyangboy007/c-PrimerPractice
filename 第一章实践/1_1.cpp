2a067daaf92e99dbd27c8b71d57144fdd502891d
birdlovefly <641993620@qq.com>
2021-12-05 11:24:33 +0800

    c++练习1_1 基本的输入输出

---
+++ 第一章实践/1_1.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2021/12/5.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "please input two numbers: "<< std::endl;
+    int a = 0, b = 0;
+    std::cin >> a >> b;
+    std::cout << "you input two numbers are  " << a << "  and  " << b << std::endl;
+    std::cout << "they are sum is  " << a + b << std::endl;
+    return 0;
+}
